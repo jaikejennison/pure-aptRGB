8c30b83a8720ab577268776ce097dbac49e8b0ed
Jaike Howard <mentor54@mac.com>
2015-12-18 20:16:57 -0500

    added feature to start.sh

--- core/start.sh
+++ core/start.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 clear
 echo -e '\e[1;31m---------\e[0m';
-echo -e '\e[0;37m main menu:\e[0m';
-echo -e '\e[1;31m--------------------------------------------------------------\e[0m';
-
-echo -e '\e[1;31m
-      --------------------------------------------------
-      ---                 pure-apt                   ---
-      ---                   v2.2                     ---
-      ---   Ubuntu package installer and tool kit!   ---
-      --------------------------------------------------
-
-      -------
-      options:
-      --------------------------------------------------
-      ---    Command    Description                  ---
-      ---    -------------------------------------   ---
-      ---      about  : all about pure-apt           ---
-      ---      update : updates for Ubuntu           ---
-      ---      search : search/instsall software     ---
-      ---      list   : display installed apps       ---
-      ---      quit   : quit pure-apt                ---
-      --------------------------------------------------
-
---------------------------------------------------------------';
+echo -e '\e[0;37mmain menu:\e[0m';
+echo -e '\e[1;31m--------------------------------------------------------------------------------\e[0m';
+echo -e '\e[1;31m\e[0m';
+echo -e '\e[1;31m\e[0m';
+echo -e '\e[1;31m      --------------------------------------------------\e[0m';
+echo -e '\e[1;31m      ---                 pure-apt                   ---\e[0m';
+echo -e '\e[1;31m      ---                   v2.2                     ---\e[0m';
+echo -e '\e[1;31m      ---   Ubuntu package installer and tool kit!   ---\e[0m';
+echo -e '\e[1;31m      --------------------------------------------------\e[0m';
+echo -e '\e[1;31m\e[0m';
+echo -e '\e[1;31m      -------\e[0m';
+echo -e '\e[0;37m      options:\e[0m';
+echo -e '\e[1;31m      --------------------------------------------------\e[0m';
+echo -e '\e[1;31m      ---    Command    Description                  ---\e[0m';
+echo -e '\e[1;31m      ---    -------------------------------------   ---\e[0m';
+echo -e '\e[1;31m      ---      about  : all about pure-apt           ---\e[0m';
+echo -e '\e[1;31m      ---      update : updates for Ubuntu           ---\e[0m';
+echo -e '\e[1;31m      ---      search : search/instsall software     ---\e[0m';
+echo -e '\e[1;31m      ---      list   : display installed apps       ---\e[0m';
+echo -e '\e[1;31m      ---      quit   : quit pure-apt                ---\e[0m';
+echo -e '\e[1;31m      --------------------------------------------------\e[0m';
+echo -e '\e[1;31m\e[0m';
+echo -e '\e[1;31m--------------------------------------------------------------------------------\e[0m';
 ./director.sh
 #-------------------------------------------------------------------------------
 # Written By: Jaike Howard copyleft 2009
