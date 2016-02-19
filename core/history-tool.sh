e06e33b7da0d686f9266a9423aa88329c7fb5502
Jaike Howard <mentor54@mac.com>
2016-02-19 17:29:48 -0500

    removing show-apt

---
+++ core/history-tool.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "====================================================="
+echo "= A Stub for future implementation to view info-apt =" 
+echo "= and  search-apt  history logs in some useful way. ="
+echo "====================================================="
+echo "Stubbed out: early 2016"
